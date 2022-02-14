30191e5b06655f4569a2f84a7f24221923711b65
javadong <dongshaowei1201@gmail.com>
2022-02-14 15:08:24 +0800

    消费者失败重试机制

--- consumer/src/main/java/cn/itcast/mq/listener/SpringRabbitListener.java
+++ consumer/src/main/java/cn/itcast/mq/listener/SpringRabbitListener.java
@@ -1,13 +1,19 @@
 package cn.itcast.mq.listener;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class SpringRabbitListener {
 
     @RabbitListener(queues = "simple.queue")
     public void listenSimpleQueue(String msg) {
-        System.out.println("消费者接收到simple.queue的消息：【" + msg + "】");
+        log.debug("消费者接收到simple.queue的消息：【" + msg + "】");
+
+        System.out.println(1 / 0);
+
+        log.info("消费者处理消息成功");
     }
 }
